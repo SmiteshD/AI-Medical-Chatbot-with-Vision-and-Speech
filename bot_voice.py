921f6b9bf2f91625ef68c1f61e16d736bfb0140a
Smitesh Dutta <smiteshdutta@gmail.com>
2025-05-13 15:58:52 +0530

    bot_voice.py

--- bot_voice.py
+++ bot_voice.py
@@ -52,3 +52,5 @@ def text_to_speech_with_elevenlabs(input_text, output_filepath):
             raise OSError("Unsupported operating system")
     except Exception as e:
         print(f"An error occurred while trying to play the audio: {e}")
+
+#can use any one of them gtts or elevenlabs
